c6ee9076541ffec47d4f8499980add6d14b001ae
mouhsin <ghoraf.mouhsin@gmail.com>
2022-07-07 21:30:25 +0200

    modify url api

--- src/main/java/com/meteo/utils/MeteocheckApi.java
+++ src/main/java/com/meteo/utils/MeteocheckApi.java
@@ -8,6 +8,7 @@ import com.meteo.services.meteo.MeteoService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -26,15 +27,15 @@ public class MeteocheckApi {
     @Autowired
     MeteoService meteoService;
 
-    private static final String key="741820df3923c728e121957b268a43b3b270b90785ae28a4725ff671bd65a3c0";
-    private static final String api="https://api.meteo-concept.com/api/forecast/nextHours?token=741820df3923c728e121957b268a43b3b270b90785ae28a4725ff671bd65a3c0&insee=93031";
+    @Autowired
+    private Environment env;
+
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
     Date date = new Date();
 
     @Scheduled(fixedRate = 30000)
     public void reportCurrentTime() {
         log.info("Début traitement le {}", dateFormat.format(new Date()));
-        System.out.println("salut c'est moi");
         try {
             Thread.sleep(30);
             MeteoEntity val=getValueByApi();
@@ -54,7 +55,8 @@ public class MeteocheckApi {
     }
     private static final String USER_AGENT = "Mozilla/5.0";
     private  void sendGET(MeteoEntity meteoEntity) throws IOException {
-        URL obj = new URL(api);
+        String urlApi=env.getProperty("api.url")+env.getProperty("api.token")+"&insee=93031";
+        URL obj = new URL(urlApi);
         HttpURLConnection con = (HttpURLConnection) obj.openConnection();
         con.setRequestMethod("GET");
         con.setRequestProperty("User-Agent", USER_AGENT);
